13f50e493cb8cd6c8002faa2284739fc25eda557
MuqhtadeerM <muhammedmuzawar9@gmail.com>
2026-04-04 00:31:56 +0530

    updated overview

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -3,17 +3,16 @@ import PageTabs from "../components/shared/PageTabs";
 import SummaryCards from "../components/overview/SummaryCards";
 import SpendingChart from "../components/overview/SpendingChart";
 import TransactionHistory from "../components/overview/TransactionHistory";
+import YourCards from "../components/overview/YourCards";
 
 export default function Overview() {
   return (
-    <Box>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {/* Tabs */}
       <PageTabs />
 
-      {/* Summary cards */}
-      <Box sx={{ mb: 3 }}>
-        <SummaryCards />
-      </Box>
+      {/* 3 summary cards */}
+      <SummaryCards />
 
       {/* Spending chart + Transaction history */}
       <Grid container spacing={2}>
@@ -24,6 +23,9 @@ export default function Overview() {
           <TransactionHistory />
         </Grid>
       </Grid>
+
+      {/* Credit cards */}
+      <YourCards />
     </Box>
   );
 }
